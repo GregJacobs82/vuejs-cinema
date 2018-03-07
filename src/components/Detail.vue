e3294378209f5b63c09d3630886be03dcb344fea
Greg <greg@2book.com>
2018-03-07 10:20:19 -0500

    starting slots, video18_1, lecture 108?
    Problem from video17_9 (lecture 106?) - where binding the movie-item
    component within Detail.vue … must also bind sessions filter?

---
+++ src/components/Detail.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="detail" v-if="movie">
+		<movie-item v-bind:movie="movie" v-bind:sessions="[]">
+			<div class="movie-details">
+				<p class="movie-genre">
+					{{ movie.Genre }}
+				</p>
+			</div>
+		</movie-item>
+	</div>
+</template>
+<script>
+	import MovieItem from './MovieItem.vue';
+	export default {
+		props: [
+			'movies'
+		],
+		computed: {
+			movie() {
+				let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+				return movie ? movie.movie : null;
+			}
+		},
+		components: {
+			MovieItem
+		}
+	}
+</script>
